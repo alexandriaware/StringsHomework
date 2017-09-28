d54a9121b179e0a5d205ae1a67cfc732ae15198e
alexandriaware <ali.mercer92@gmail.com>
2017-09-28 15:12:14 -0400

    problem one

--- StringsHomework/Program.cs
+++ StringsHomework/Program.cs
@@ -10,6 +10,25 @@ namespace StringsHomework
     {
         static void Main(string[] args)
         {
+            //Problem 1
+            string firstName = "Ali";
+            string lastName = "Ware";
+
+            Console.WriteLine(firstName.Length);
+            Console.WriteLine(lastName.Length);
+
+            if (firstName.Length > lastName.Length)
+            {
+                Console.WriteLine("First is longer.");
+            }
+            else if (firstName.Length == lastName.Length)
+            {
+                Console.WriteLine("Sameses!");
+            }
+            else
+            {
+                Console.WriteLine("Last must be longer!");
+            }
         }
     }
 }
