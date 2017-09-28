2916c1cd1fbbcaab9fab322300d6ece9def046ac
alexandriaware <ali.mercer92@gmail.com>
2017-09-28 16:02:40 -0400

    Problem Two

--- StringsHomework/Program.cs
+++ StringsHomework/Program.cs
@@ -10,25 +10,34 @@ namespace StringsHomework
     {
         static void Main(string[] args)
         {
-            //Problem 1
-            string firstName = "Ali";
-            string lastName = "Ware";
+            ////Problem 1
+            //string firstName = "Ali";
+            //string lastName = "Ware";
 
-            Console.WriteLine(firstName.Length);
-            Console.WriteLine(lastName.Length);
+            //Console.WriteLine(firstName.Length);
+            //Console.WriteLine(lastName.Length);
 
-            if (firstName.Length > lastName.Length)
-            {
-                Console.WriteLine("First is longer.");
-            }
-            else if (firstName.Length == lastName.Length)
-            {
-                Console.WriteLine("Sameses!");
-            }
-            else
-            {
-                Console.WriteLine("Last must be longer!");
-            }
+            //if (firstName.Length > lastName.Length)
+            //{
+            //    Console.WriteLine("First is longer.");
+            //}
+            //else if (firstName.Length == lastName.Length)
+            //{
+            //    Console.WriteLine("Sameses!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Last must be longer!");
+            //}
+
+
+
+            //Problem 2
+            Console.WriteLine("This is a tab \t.");
+            Console.WriteLine("This is a \"quote\".");
+            Console.WriteLine("this is a backslash\\");
+            Console.WriteLine("This is a \n new line.");
+            Console.WriteLine("And this is a bell. \a");
         }
     }
 }
