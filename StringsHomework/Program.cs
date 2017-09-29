a8faba4497cb7a7f867c1293ce44811140f6e91d
alexandriaware <ali.mercer92@gmail.com>
2017-09-29 09:22:11 -0400

    Corrected Problem 4

--- StringsHomework/Program.cs
+++ StringsHomework/Program.cs
@@ -70,35 +70,37 @@ namespace StringsHomework
 
 
             ////Problem 4
-            //Console.WriteLine("Please enter your first name");
-            //string firstName = Console.ReadLine();
+            Console.WriteLine("Please enter your first name");
+            string firstName = Console.ReadLine();
 
-            //Console.WriteLine("Please enter your last name");
-            //string lastName = Console.ReadLine();
+            Console.WriteLine("Please enter your last name");
+            string lastName = Console.ReadLine();
 
             //Console.WriteLine(firstName + " " + lastName);
 
-            ////pt2 is not supposed to work....you can't concatenate using .Equals()
+            //pt2
+            string fullName = string.Concat(firstName, " ", lastName);
+            Console.WriteLine(fullName);
 
 
 
 
-            //Problem 5
-            Console.WriteLine("What kind of pet is the opposite of a dog?");
-            string answer = Console.ReadLine().ToLower();
+            ////Problem 5
+            //Console.WriteLine("What kind of pet is the opposite of a dog?");
+            //string answer = Console.ReadLine().ToLower();
 
-            if (answer == "cat")
-            {
-                Console.WriteLine("You guessed correctly!");
-            }
-            else if (answer == "meow") 
-            {
-                Console.WriteLine("You guessed correctly!");
-            }
-            else
-            {
-                Console.WriteLine("You are incorrect.");
-            }
+            //if (answer == "cat")
+            //{
+            //    Console.WriteLine("You guessed correctly!");
+            //}
+            //else if (answer == "meow") 
+            //{
+            //    Console.WriteLine("You guessed correctly!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("You are incorrect.");
+            //}
 
 
 
